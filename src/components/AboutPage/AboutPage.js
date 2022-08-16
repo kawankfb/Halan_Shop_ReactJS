93df9e6f12a9ec515a475363f2f5a6ea8bd73787
kawankfb <kawan.kfb@gmail.com>
2022-08-16 19:14:44 +0430

    updated abou page to create cards based on given array

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -2,14 +2,53 @@ import React from "react";
 import "./AboutPage.css";
 
 const AboutPage = (props) => {
+
+    function getAbouts(){
+        const abouts =[
+            {
+                text: "halan",
+                imageSrc:"/logo.png",
+                alt:"halan"},
+            {
+                text: "kawan",
+                imageSrc:"/logo.png",
+                alt:"kawan"},
+            {
+                text: "afshin",
+                imageSrc:"/logo.png",
+                alt:"afshin"}
+        ];
+        return abouts;
+    }
+
+    
 return (
     <div>
-        <h1 className="description-text">
-            Hi, this is our about page
-        </h1>
+        <ul>
+            {getAbouts().map((card,index) => {
+                console.log(card);
+  return (
+      <DescriptionDiv text={card.text} imageSrc={card.imageSrc} alt={card.alt} index={index} />
+      
+  ) 
+})}
+        </ul>
     </div>
     
 );
 }
+
+function DescriptionDiv({text,imageSrc, alt,index, ...props }) {
+    return (
+      <li className="description-card">
+        <div className={index%2===0? "even-card" : "odd-card"}>
+            <p >
+                {text}
+            </p>
+            
+        </div>
+      </li>
+    )
+  }
 
 export default AboutPage
