d87e70f6501a08b51437ae48f9db82d0611cd529
kawankfb <kawan.kfb@gmail.com>
2022-08-17 01:08:09 +0430

    updated about page to have some cards to show as preview

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -7,15 +7,15 @@ const AboutPage = (props) => {
         const abouts =[
             {
                 text: "halan",
-                imageSrc:"/logo.png",
+                imageSrc:process.env.PUBLIC_URL +"/logo.png",
                 alt:"halan"},
             {
                 text: "kawan",
-                imageSrc:"/logo.png",
+                imageSrc:process.env.PUBLIC_URL +"/images/kawan.jpeg",
                 alt:"kawan"},
             {
                 text: "afshin",
-                imageSrc:"/logo.png",
+                imageSrc:process.env.PUBLIC_URL +"/images/afshin.jpg",
                 alt:"afshin"}
         ];
         return abouts;
@@ -24,7 +24,7 @@ const AboutPage = (props) => {
     
 return (
     <div>
-        <ul>
+        <ul className="about-ul">
             {getAbouts().map((card,index) => {
                 console.log(card);
   return (
@@ -42,9 +42,13 @@ function DescriptionDiv({text,imageSrc, alt,index, ...props }) {
     return (
       <li className="description-card">
         <div className={index%2===0? "even-card" : "odd-card"}>
-            <p >
+
+        <img className="description-image" src={imageSrc} alt={alt}/>
+
+            <p className="description-text">
                 {text}
             </p>
+
             
         </div>
       </li>
