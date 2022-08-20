e6a0d5fa2e732369cd8a854dff3b6695433317c9
kawankfb <kawan.kfb@gmail.com>
2022-08-20 19:27:42 +0430

    added the ability to make API calls using axios to the homePage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
 import "./HomePage.css";
+import axios from 'axios';
 
 const HomePage = (props) => {
+
+    const[productsArray, setProductsArray]= useState([]);
+
+    useEffect(() => {
+        getAllProducts();
+   }, []);
+
+    const getAllProducts = () =>{
+        axios.get("http://127.0.0.1:80/api/products", {
+            params: {
+              page:1
+            }
+          })
+          .then(function (response) {
+            setProductsArray(response.data.data.list);
+            console.log(response);
+          })
+          .catch(function (error) {
+            setProductsArray([]);
+          })
+          .then(function () {
+            // always executed
+          });
+    }
+    
 return (
     <div>
         <h1 className="description-text">
             Hi, this is our home page
         </h1>
+        <ul>
+            {productsArray.map(product=><li>{product.name}</li>)}
+        </ul>
     </div>
     
 );
