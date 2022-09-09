9e03c41d1cd3194af9267da6685db17b3df3aac3
kawankfb <kawan.kfb@gmail.com>
2022-09-09 16:21:37 +0430

    made a few changes to index page

--- src/components/IndexProductsPage/IndexProductsPage.js
+++ src/components/IndexProductsPage/IndexProductsPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./IndexProductsPage.css";
 import { useLocation } from "react-router-dom";
 
@@ -8,13 +8,59 @@ const IndexProductsPage = (props) => {
     let query = React.useMemo(() => new URLSearchParams(search), [search]);
     let sort_by=query.get("sort_by");
     let page=query.get("page");
-return (
-    <div>
-       the chosen sort by is: sort by &quot;{sort_by}&quot; <br/>
-       and the page number is: page &quot;{page}&quot; 
-    </div>
-    
-);
+
+    const[mobileMode, setMobileMode]= useState(false);
+
+    useEffect(()=>{
+        if(window.innerWidth > 719){
+            setMobileMode(false);
+        }
+        else if(window.innerWidth <= 719){
+            setMobileMode(true);
+        }
+    },[]);
+
+    useEffect(()=>{
+        const handleResize = () =>{
+            if(window.innerWidth > 719){
+                setMobileMode(false);
+            }
+            else if(window.innerWidth <= 719){
+                setMobileMode(true);
+            }
+        };
+        window.addEventListener('resize',handleResize);
+
+        return ()=> {
+            window.removeEventListener('resize',handleResize);
+        }
+
+    },[]);
+    if(mobileMode)
+        return (
+            <div className="index-page-container">
+            <div className="filter-bar">
+                <div className="filter-bar-button">
+                    <h3 className="filter-bar-button-text"> filter</h3>
+                    <img className="filter-bar-button-icon" src={process.env.PUBLIC_URL +"/icons/left-arrow.svg"}></img>
+                </div>
+
+
+                <div className="filter-bar-button">
+                    <h3 className="filter-bar-button-text"> sort by</h3>
+                    <img className="filter-bar-button-icon" src={process.env.PUBLIC_URL +"/icons/left-arrow.svg"}></img>
+                </div>
+            </div>
+            </div>
+           
+
+        );
+    else
+        return (
+            <div>
+                not mobile
+            </div>
+        );
 }
 
 export default IndexProductsPage
